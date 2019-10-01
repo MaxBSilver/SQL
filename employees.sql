1f82f7827bd0d3770e0ec494fd4f180767615979
Max Silver <Maxbsilver@gmail.com>
2019-10-01 14:28:23 -0600

    Employees table practicing 1. Required fields (using NOT NULL) 2. Primary Keys (with AUTO_INCREMENT)

---
+++ employees.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  middle_name VARCHAR(255),
+  age INT NOT NULL,
+  curreent_status VARCHAR(255),
+  PRIMARY KEY(id)
+)
